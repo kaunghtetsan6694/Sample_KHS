3799babedfa1c27f62839f79df85415bfba09ed2
Kaung Htet San <43808051+kaunghtetsan6694@users.noreply.github.com>
2021-08-04 17:48:57 +0630

    Add project files.

---
+++ Sample_KHS/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Sample_KHS.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
